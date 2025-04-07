1a0fef45de39ec9f4f3285e32d009ef1ec933942
joona95 <choijuna052@gmail.com>
2025-04-07 20:32:29 +0900

    feat: 상품 재고 도메인 클래스 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/item/Stock.java
@@ -0,0 +1,59 @@
+package kr.hhplus.be.server.domain.item;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+public class Stock {
+
+    private int count;
+
+    public static Stock of(int count) {
+        return new Stock(count);
+    }
+
+    public Stock(int count) {
+
+        if (count < 0) {
+            throw new IllegalArgumentException("재고는 음수가 될 수 없습니다.");
+        }
+
+        this.count = count;
+    }
+
+    public Stock decrease(int count) {
+
+        if (count < 0) {
+            throw new IllegalArgumentException("재고 차감하려는 값은 0 이상이어야 합니다.");
+        }
+
+        return Stock.of(this.count - count);
+    }
+
+    public Stock increase(int count) {
+
+        if (count < 0) {
+            throw new IllegalArgumentException("재고 증가하려는 값은 0 이상이어야 합니다.");
+        }
+
+        return Stock.of(this.count + count);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Stock stock = (Stock) o;
+        return count == stock.count;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(count);
+    }
+}
