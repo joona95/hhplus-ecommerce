09f84b58d07906077c2629daaed6034891552f7c
joona95 <choijuna052@gmail.com>
2025-04-08 18:43:28 +0900

    feat: 금액 도메인 클래스 추가

---
+++ src/main/java/kr/hhplus/be/server/domain/point/Amount.java
@@ -0,0 +1,59 @@
+package kr.hhplus.be.server.domain.point;
+
+import jakarta.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+public class Amount {
+
+    private int value;
+
+    public static Amount of(int value) {
+        return new Amount(value);
+    }
+
+    public Amount(int value) {
+
+        if (value < 0) {
+            throw new IllegalArgumentException("금액은 음수일 수 없습니다.");
+        }
+
+        this.value = value;
+    }
+
+    public Amount plus(int value) {
+
+        if (value < 0) {
+            throw new IllegalArgumentException("충전할 금액은 음수일 수 없습니다.");
+        }
+
+        return Amount.of(this.value + value);
+    }
+
+    public Amount minus(int value) {
+
+        if (value < 0) {
+            throw new IllegalArgumentException("사용할 금액은 음수일 수 없습니다.");
+        }
+
+        return Amount.of(this.value - value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Amount amount = (Amount) o;
+        return value == amount.value;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+}
