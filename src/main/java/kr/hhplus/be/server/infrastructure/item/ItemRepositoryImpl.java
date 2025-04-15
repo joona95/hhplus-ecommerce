aae604b4ed145bde60bc42d75b9f65725354ffac
joona95 <choijuna052@gmail.com>
2025-04-15 21:55:43 +0900

    feat: Item 레포지토리 구현

---
+++ src/main/java/kr/hhplus/be/server/infrastructure/item/ItemRepositoryImpl.java
@@ -0,0 +1,42 @@
+package kr.hhplus.be.server.infrastructure.item;
+
+import kr.hhplus.be.server.domain.item.Item;
+import kr.hhplus.be.server.domain.item.ItemRepository;
+import kr.hhplus.be.server.domain.item.PopularItem;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public class ItemRepositoryImpl implements ItemRepository {
+
+    private final ItemJpaRepository itemJpaRepository;
+    private final PopularItemJpaRepository popularItemJpaRepository;
+
+    public ItemRepositoryImpl(ItemJpaRepository itemJpaRepository, PopularItemJpaRepository popularItemJpaRepository) {
+        this.itemJpaRepository = itemJpaRepository;
+        this.popularItemJpaRepository = popularItemJpaRepository;
+    }
+
+    @Override
+    public Optional<Item> findById(long id) {
+        return itemJpaRepository.findById(id);
+    }
+
+    @Override
+    public List<PopularItem> findPopularItems() {
+        return popularItemJpaRepository.findByOrderDateAfter(LocalDate.now().minusDays(3));
+    }
+
+    @Override
+    public List<Item> findByIdIn(List<Long> ids) {
+        return itemJpaRepository.findAllById(ids);
+    }
+
+    @Override
+    public List<PopularItem> savePopularItems(List<PopularItem> popularItems) {
+        return popularItemJpaRepository.saveAll(popularItems);
+    }
+}
---
+++ src/main/java/kr/hhplus/be/server/infrastructure/item/PopularItemJpaRepository.java
@@ -0,0 +1,11 @@
+package kr.hhplus.be.server.infrastructure.item;
+
+import kr.hhplus.be.server.domain.item.PopularItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface PopularItemJpaRepository extends JpaRepository<PopularItem, Long> {
+    List<PopularItem> findByOrderDateAfter(LocalDate orderDateAfter);
+}
