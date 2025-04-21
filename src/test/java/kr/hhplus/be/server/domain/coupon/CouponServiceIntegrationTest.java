1df993209b83ce9cc876674c70c6ca0f29541248
joona95 <choijuna052@gmail.com>
2025-04-21 22:30:50 +0900

    test: Coupon 에 User 연관관계 추가에 따른 테스트코드 수정

--- src/test/java/kr/hhplus/be/server/domain/coupon/CouponServiceIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/domain/coupon/CouponServiceIntegrationTest.java
@@ -2,11 +2,14 @@ package kr.hhplus.be.server.domain.coupon;
 
 import kr.hhplus.be.server.domain.order.Order;
 import kr.hhplus.be.server.domain.order.OrderAmountInfo;
+import kr.hhplus.be.server.domain.user.User;
 import kr.hhplus.be.server.fixtures.CouponFixtures;
 import kr.hhplus.be.server.fixtures.OrderFixtures;
+import kr.hhplus.be.server.fixtures.UserFixtures;
 import kr.hhplus.be.server.infrastructure.coupon.CouponIssueJpaRepository;
 import kr.hhplus.be.server.infrastructure.coupon.CouponJpaRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -34,55 +37,66 @@ public class CouponServiceIntegrationTest {
         couponJpaRepository.deleteAll();
     }
 
-    @Test
-    void 쿠폰_발급() {
+    @Nested
+    class 쿠폰_발급 {
 
-        // given
-        Coupon coupon = couponJpaRepository.save(CouponFixtures.정상_쿠폰_생성());
+        @Test
+        void 정상적으로_쿠폰_발급() {
 
-        CouponIssueCommand command = new CouponIssueCommand(1L, coupon.getId());
+            // given
+            User user = UserFixtures.식별자로_유저_생성(1L);
+            Coupon coupon = couponJpaRepository.save(CouponFixtures.정상_쿠폰_생성());
 
-        // when
-        CouponIssue couponIssue = couponService.issueCoupon(command);
+            CouponIssueCommand command = new CouponIssueCommand(coupon.getId());
 
-        // then
-        assertThat(couponIssue).isNotNull();
-        assertThat(couponIssue.getUserId()).isEqualTo(1L);
-        assertThat(couponIssue.getCouponId()).isEqualTo(coupon.getId());
+            // when
+            CouponIssue couponIssue = couponService.issueCoupon(user, command);
+
+            // then
+            assertThat(couponIssue).isNotNull();
+            assertThat(couponIssue.getCouponId()).isEqualTo(coupon.getId());
+        }
+
     }
 
-    @Test
-    void 정액_할인_쿠폰_적용() {
+    @Nested
+    class 쿠폰_할인_적용 {
 
-        // given
-        Order order = OrderFixtures.주문가격정보로_주문_생성(OrderAmountInfo.of(10000, 10000, 0));
-        Coupon coupon = couponJpaRepository.save(CouponFixtures.정액_할인_쿠폰_생성(1000));
-        couponIssueJpaRepository.save(CouponIssue.of(1L, coupon));
+        @Test
+        void 정액_할인_쿠폰_적용() {
 
-        CouponApplyCommand command = new CouponApplyCommand(order, coupon.getId());
+            // given
+            User user = UserFixtures.식별자로_유저_생성(1L);
+            Order order = OrderFixtures.주문가격정보로_주문_생성(OrderAmountInfo.of(10000, 10000, 0));
+            Coupon coupon = couponJpaRepository.save(CouponFixtures.정액_할인_쿠폰_생성(1000));
+            couponIssueJpaRepository.save(CouponIssue.of(user, coupon));
 
-        // when
-        int discountAmount = couponService.applyCoupon(command);
+            CouponApplyCommand command = new CouponApplyCommand(order, coupon.getId());
 
-        // then
-        assertThat(discountAmount).isEqualTo(1000);
-    }
+            // when
+            int discountAmount = couponService.applyCoupon(command);
 
-    @Test
-    void 정률_할인_쿠폰_적용() {
+            // then
+            assertThat(discountAmount).isEqualTo(1000);
+        }
 
-        // given
-        Order order = OrderFixtures.주문가격정보로_주문_생성(OrderAmountInfo.of(10000, 10000, 0));
-        Coupon coupon = couponJpaRepository.save(CouponFixtures.정률_할인_쿠폰_생성(10));
-        couponIssueJpaRepository.save(CouponIssue.of(1L, coupon));
+        @Test
+        void 정률_할인_쿠폰_적용() {
 
-        CouponApplyCommand command = new CouponApplyCommand(order, coupon.getId());
+            // given
+            User user = UserFixtures.식별자로_유저_생성(1L);
+            Order order = OrderFixtures.주문가격정보로_주문_생성(OrderAmountInfo.of(10000, 10000, 0));
+            Coupon coupon = couponJpaRepository.save(CouponFixtures.정률_할인_쿠폰_생성(10));
+            couponIssueJpaRepository.save(CouponIssue.of(user, coupon));
 
-        // when
-        int discountAmount = couponService.applyCoupon(command);
+            CouponApplyCommand command = new CouponApplyCommand(order, coupon.getId());
 
-        // then
-        assertThat(discountAmount).isEqualTo(1000);
+            // when
+            int discountAmount = couponService.applyCoupon(command);
+
+            // then
+            assertThat(discountAmount).isEqualTo(1000);
+        }
     }
 
 }
