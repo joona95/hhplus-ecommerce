0d7ba6f911bd70c4cba411491772571bef715bb0
joona95 <choijuna052@gmail.com>
2025-04-21 23:24:18 +0900

    test: Order 에 User 연관관계 추가에 따른 테스트코드 수정

--- src/test/java/kr/hhplus/be/server/domain/order/OrderServiceTest.java
+++ src/test/java/kr/hhplus/be/server/domain/order/OrderServiceTest.java
@@ -2,6 +2,8 @@ package kr.hhplus.be.server.domain.order;
 
 import kr.hhplus.be.server.domain.item.Item;
 import kr.hhplus.be.server.domain.item.Stock;
+import kr.hhplus.be.server.domain.user.User;
+import kr.hhplus.be.server.fixtures.UserFixtures;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
@@ -34,6 +36,7 @@ class OrderServiceTest {
     @BeforeEach
     void setUp() {
 
+        User user = UserFixtures.식별자로_유저_생성(1L);
         List<Item> items = List.of(
                 Item.of(1L, "상품명1", Stock.of(10), 10000),
                 Item.of(2L, "상품명2", Stock.of(10), 20000)
@@ -44,9 +47,9 @@ class OrderServiceTest {
                 OrderItemCreateCommand.of(items.get(1), 2)
         );
 
-        command = OrderCreateCommand.of(1L, itemCommands);
+        command = OrderCreateCommand.of(user, itemCommands);
 
-        order = new Order(1L, 1L, 1L, OrderStatus.COMPLETE, OrderAmountInfo.of(30000, 50000, 20000), LocalDateTime.now(), LocalDateTime.now());
+        order = new Order(1L, user, 1L, OrderStatus.COMPLETE, OrderAmountInfo.of(30000, 50000, 20000), LocalDateTime.now(), LocalDateTime.now());
 
         orderItems = List.of(
                 OrderItem.of(order, items.get(0), 1),
@@ -60,7 +63,9 @@ class OrderServiceTest {
         void 주문_저장_레포지토리_1회_호출() {
 
             //given
-            when(orderRepository.saveOrder(Order.of(command.userId())))
+            User user = UserFixtures.식별자로_유저_생성(1L);
+
+            when(orderRepository.saveOrder(Order.of(user)))
                     .thenReturn(order);
 
             when(orderRepository.saveOrderItems(orderItems))
@@ -70,14 +75,16 @@ class OrderServiceTest {
             orderService.createOrder(command);
 
             //then
-            verify(orderRepository, times(1)).saveOrder(Order.of(command.userId()));
+            verify(orderRepository, times(1)).saveOrder(Order.of(user));
         }
 
         @Test
         void 주문_상품_목록_저장_레포지토리_1회_호출() {
 
             //given
-            when(orderRepository.saveOrder(Order.of(command.userId())))
+            User user = UserFixtures.식별자로_유저_생성(1L);
+
+            when(orderRepository.saveOrder(Order.of(user)))
                     .thenReturn(order);
 
             when(orderRepository.saveOrderItems(orderItems))
@@ -94,7 +101,9 @@ class OrderServiceTest {
         void 주문_저장_실패_시_주문_상품_목록_저장_레포지토리_0회_호출() {
 
             //given
-            when(orderRepository.saveOrder(Order.of(command.userId())))
+            User user = UserFixtures.식별자로_유저_생성(1L);
+
+            when(orderRepository.saveOrder(Order.of(user)))
                     .thenThrow(RuntimeException.class);
 
             //when, then
--- src/test/java/kr/hhplus/be/server/fixtures/OrderFixtures.java
+++ src/test/java/kr/hhplus/be/server/fixtures/OrderFixtures.java
@@ -4,29 +4,30 @@ import kr.hhplus.be.server.domain.order.Order;
 import kr.hhplus.be.server.domain.order.OrderAmountInfo;
 import kr.hhplus.be.server.domain.order.OrderItem;
 import kr.hhplus.be.server.domain.order.OrderStatus;
+import kr.hhplus.be.server.domain.user.User;
 
 import java.time.LocalDateTime;
 
 public class OrderFixtures {
 
     public static Order 정상_주문_생성() {
-        return new Order(null, 1L, 1L, OrderStatus.COMPLETE, OrderAmountInfo.of(30000, 50000, 20000), LocalDateTime.now(), LocalDateTime.now());
+        return new Order(null, UserFixtures.식별자로_유저_생성(1L), 1L, OrderStatus.COMPLETE, OrderAmountInfo.of(30000, 50000, 20000), LocalDateTime.now(), LocalDateTime.now());
     }
 
-    public static Order 유저식별자로_주문_생성(long userId) {
-        return new Order(null, userId, 1L, OrderStatus.COMPLETE, OrderAmountInfo.of(30000, 50000, 20000), LocalDateTime.now(), LocalDateTime.now());
+    public static Order 유저로_주문_생성(User user) {
+        return new Order(null, user, 1L, OrderStatus.COMPLETE, OrderAmountInfo.of(30000, 50000, 20000), LocalDateTime.now(), LocalDateTime.now());
     }
 
     public static Order 주문상태로_주문_생성(OrderStatus orderStatus) {
-        return new Order(null, 1L, 1L, orderStatus, OrderAmountInfo.of(30000, 50000, 20000), LocalDateTime.now(), LocalDateTime.now());
+        return new Order(null, UserFixtures.식별자로_유저_생성(1L), 1L, orderStatus, OrderAmountInfo.of(30000, 50000, 20000), LocalDateTime.now(), LocalDateTime.now());
     }
 
     public static Order 주문가격정보로_주문_생성(OrderAmountInfo orderAmountInfo) {
-        return new Order(null, 1L, 1L, OrderStatus.COMPLETE, orderAmountInfo, LocalDateTime.now(), LocalDateTime.now());
+        return new Order(null, UserFixtures.식별자로_유저_생성(1L), 1L, OrderStatus.COMPLETE, orderAmountInfo, LocalDateTime.now(), LocalDateTime.now());
     }
 
     public static Order 생성일시로_주문_생성(LocalDateTime createdAt) {
-        return new Order(null, 1L, 1L, OrderStatus.COMPLETE, OrderAmountInfo.of(30000, 50000, 20000), createdAt, createdAt);
+        return new Order(null, UserFixtures.식별자로_유저_생성(1L), 1L, OrderStatus.COMPLETE, OrderAmountInfo.of(30000, 50000, 20000), createdAt, createdAt);
     }
 
     public static OrderItem 주문식별자로_주문상품_생성(long orderId) {
