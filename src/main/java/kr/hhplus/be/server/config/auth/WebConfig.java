5bcfc12da22aa612c633d84cb58eabfcbf33fc53
joona95 <choijuna052@gmail.com>
2025-04-25 00:26:18 +0900

    feat: 예외 로깅 인터셉터 추가

--- src/main/java/kr/hhplus/be/server/config/auth/WebConfig.java
+++ src/main/java/kr/hhplus/be/server/config/auth/WebConfig.java
@@ -1,8 +1,10 @@
 package kr.hhplus.be.server.config.auth;
 
 import kr.hhplus.be.server.common.auth.AuthUserArgumentResolver;
+import kr.hhplus.be.server.common.support.ErrorHandlingInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.List;
@@ -11,13 +13,20 @@ import java.util.List;
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthUserArgumentResolver authUserArgumentResolver;
+    private final ErrorHandlingInterceptor errorHandlingInterceptor;
 
-    public WebConfig(AuthUserArgumentResolver authUserArgumentResolver) {
+    public WebConfig(AuthUserArgumentResolver authUserArgumentResolver, ErrorHandlingInterceptor errorHandlingInterceptor) {
         this.authUserArgumentResolver = authUserArgumentResolver;
+        this.errorHandlingInterceptor = errorHandlingInterceptor;
     }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(authUserArgumentResolver);
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(errorHandlingInterceptor).addPathPatterns("/**");
     }
 }
