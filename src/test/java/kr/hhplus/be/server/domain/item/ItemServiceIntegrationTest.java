dd5a74be4c0e152b6a86e03acf8929519145fb3c
joona95 <choijuna052@gmail.com>
2025-04-24 22:38:36 +0900

    test: 상품 재고 차감 동시성테스트 추가

--- src/test/java/kr/hhplus/be/server/domain/item/ItemServiceIntegrationTest.java
+++ src/test/java/kr/hhplus/be/server/domain/item/ItemServiceIntegrationTest.java
@@ -6,13 +6,20 @@ import kr.hhplus.be.server.fixtures.OrderFixtures;
 import kr.hhplus.be.server.infrastructure.item.ItemJpaRepository;
 import kr.hhplus.be.server.infrastructure.item.PopularItemJpaRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicInteger;
 
+import static kr.hhplus.be.server.domain.item.ItemCommand.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -34,43 +41,97 @@ public class ItemServiceIntegrationTest {
         itemJpaRepository.deleteAll();
     }
 
-    @Test
-    void 재고_차감_성공() {
+    @Nested
+    class 재고_차감 {
 
-        // given
-        Item item1 = ItemFixtures.재고로_상품_생성(Stock.of(10));
-        Item item2 = ItemFixtures.재고로_상품_생성(Stock.of(10));
+        @Test
+        void 재고_차감_성공() {
 
-        itemJpaRepository.saveAll(List.of(item1, item2));
+            // given
+            Item item1 = ItemFixtures.재고로_상품_생성(Stock.of(10));
+            Item item2 = ItemFixtures.재고로_상품_생성(Stock.of(10));
 
-        List<ItemCommand.StockDecreaseCommand> commands = List.of(
-                ItemCommand.StockDecreaseCommand.of(item1.getId(), 1),
-                ItemCommand.StockDecreaseCommand.of(item2.getId(), 2)
-        );
+            itemJpaRepository.saveAll(List.of(item1, item2));
 
-        // when
-        List<Item> result = itemService.decreaseStocks(commands);
+            List<StockDecreaseCommand> commands = List.of(
+                    StockDecreaseCommand.of(item1.getId(), 1),
+                    StockDecreaseCommand.of(item2.getId(), 2)
+            );
 
-        // then
-        assertThat(result).hasSize(2);
-        assertThat(result.get(0).getStock()).isEqualTo(9); // 재고 감소 확인
-        assertThat(result.get(1).getStock()).isEqualTo(8); // 재고 감소 확인
+            // when
+            List<Item> result = itemService.decreaseStocks(commands);
+
+            // then
+            assertThat(result).hasSize(2);
+            assertThat(result.get(0).getStock()).isEqualTo(9); // 재고 감소 확인
+            assertThat(result.get(1).getStock()).isEqualTo(8); // 재고 감소 확인
+        }
     }
 
-    @Test
-    void 인기_상품_통계_생성_시_상품식별자별로_그룹핑하여_생성() {
+    @Nested
+    class 인기_상품_통계_생성 {
 
-        // given
-        List<OrderItem> orderItems = List.of(
-                OrderFixtures.상품식별자로_주문상품_생성(1L),
-                OrderFixtures.상품식별자로_주문상품_생성(1L),
-                OrderFixtures.상품식별자로_주문상품_생성(2L)
-        );
+        @Test
+        void 인기_상품_통계_생성_시_상품식별자별로_그룹핑하여_생성() {
 
-        // when
-        List<PopularItem> result = itemService.createPopularItemStatistics(orderItems);
+            // given
+            List<OrderItem> orderItems = List.of(
+                    OrderFixtures.상품식별자로_주문상품_생성(1L),
+                    OrderFixtures.상품식별자로_주문상품_생성(1L),
+                    OrderFixtures.상품식별자로_주문상품_생성(2L)
+            );
 
-        // then
-        assertThat(result).hasSize(2);
+            // when
+            List<PopularItem> result = itemService.createPopularItemStatistics(orderItems);
+
+            // then
+            assertThat(result).hasSize(2);
+        }
     }
+
+    @Nested
+    class 동시성_테스트 {
+
+        @Test
+        void 재고_차감_시_동시_요청이_들어온_경우_요청이_들어온_만큼_재고_차감_발생() throws InterruptedException {
+
+            //given
+            Item item = itemJpaRepository.save(ItemFixtures.재고로_상품_생성(Stock.of(20)));
+
+            int threadCount = 20;
+            ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+            CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+
+            List<StockDecreaseCommand> commands = List.of(StockDecreaseCommand.of(item.getId(), 1));
+
+            AtomicInteger failureCount = new AtomicInteger();
+
+            //when
+            long startTime = System.currentTimeMillis();
+            for (int i = 0; i < threadCount; i++) {
+                executorService.execute(() -> {
+                    try {
+                        itemService.decreaseStocks(commands);
+                    } catch (Exception e) {
+                        failureCount.getAndIncrement();
+                    }
+                    countDownLatch.countDown();
+                });
+            }
+
+            countDownLatch.await();
+            long endTime = System.currentTimeMillis();
+
+            //then
+            System.out.println("실행 시간 == " + (endTime - startTime) + "ms");
+
+            Optional<Item> result = itemJpaRepository.findById(item.getId());
+
+            assertThat(result).isPresent();
+            assertThat(result.get().getStock()).isEqualTo(failureCount.get());
+
+            System.out.println("실패 횟수 : " + failureCount.get() + ", 재고 : " + result.get().getStock());
+        }
+    }
+
 }
