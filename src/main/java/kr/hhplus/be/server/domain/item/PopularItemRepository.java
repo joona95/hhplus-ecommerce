7ac05fe4acfa57a6c0a3d668e2a7eab1063b827f
joona95 <choijuna052@gmail.com>
2025-05-16 06:12:51 +0900

    feat: 스케줄러에서 전날 주문 수량 통계 저장 시 주문 테이블이 아니라 레디스 집계 정보로 반영하도록 수정

--- src/main/java/kr/hhplus/be/server/domain/item/PopularItemRepository.java
+++ src/main/java/kr/hhplus/be/server/domain/item/PopularItemRepository.java
@@ -2,10 +2,13 @@ package kr.hhplus.be.server.domain.item;
 
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface PopularItemRepository {
+
+    List<PopularItem> findPopularItemScore(LocalDate date);
 
     List<PopularItem> findPopularItems();
 
--- src/main/java/kr/hhplus/be/server/infrastructure/item/PopularItemRepositoryImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/item/PopularItemRepositoryImpl.java
@@ -3,19 +3,27 @@ package kr.hhplus.be.server.infrastructure.item;
 import kr.hhplus.be.server.domain.item.PopularItem;
 import kr.hhplus.be.server.domain.item.PopularItemRepository;
 import kr.hhplus.be.server.domain.item.PopularItemStatistics;
+import org.redisson.api.RScoredSortedSet;
+import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Repository;
 
+import java.time.Duration;
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public class PopularItemRepositoryImpl implements PopularItemRepository {
 
+    private static final String POPULAR_ITEMS_KEY_PREFIX = "popular-items:";
+
     private final PopularItemJpaRepository popularItemJpaRepository;
     private final PopularItemQuerydslRepository popularItemQuerydslRepository;
+    private final RedissonClient redissonClient;
 
-    public PopularItemRepositoryImpl(PopularItemJpaRepository popularItemJpaRepository, PopularItemQuerydslRepository popularItemQuerydslRepository) {
+    public PopularItemRepositoryImpl(PopularItemJpaRepository popularItemJpaRepository, PopularItemQuerydslRepository popularItemQuerydslRepository, RedissonClient redissonClient) {
         this.popularItemJpaRepository = popularItemJpaRepository;
         this.popularItemQuerydslRepository = popularItemQuerydslRepository;
+        this.redissonClient = redissonClient;
     }
 
     @Override
@@ -35,4 +43,11 @@ public class PopularItemRepositoryImpl implements PopularItemRepository {
         return popularItemQuerydslRepository.findPopularItems();
     }
 
+    @Override
+    public List<PopularItem> findPopularItemScore(LocalDate date) {
+        RScoredSortedSet<Long> zset = redissonClient.getScoredSortedSet(POPULAR_ITEMS_KEY_PREFIX + date);
+        return zset.entryRange(0, -1).stream()
+                .map(e -> new PopularItem(e.getValue(), e.getScore().intValue()))
+                .toList();
+    }
 }
